7a5496b179ba4e0ea98ad13519e00dd7d68280b6
Henok Asaye <henokasaye77@gmail.com>
2024-10-15 20:55:36 +0300

    modify thr cartcontroller

--- ecommerce-backend/controllers/cartController.js
+++ ecommerce-backend/controllers/cartController.js
@@ -1,75 +1,130 @@
 import Product from "../models/product.js";
 import User from "../models/user.js";
 
+// Add to Cart Function
+export const addToCart = async (req, res) => {
+    const { productId, quantity } = req.body;
 
-export const addToCart = async(req,res)=>{
-    const {productId,quantity} = req.body
-
-    try{
-         const user = await User.findById(req.user._id)
-
-         if(!user){
-            return res.staus(404).json({message:"user not found!"})
-         }
-        const findProduct =  await Product.findById(productId);
-        if(!findProduct){
-            return res.status(404).json({message:"product not found!"});
-        }
-        const existingProduct = await user.cart.find(item =>item.productId.toString()  == productId.toString());
-
-        if(existingProduct){
-            existingProduct.quantity += quantity;
-            await user.save();
-            return res.status(200).json({ message: "Product quantity updated successfully!" });
-        }
-      
-
-        user.cart.push({product:productId,quantity:quantity})
-        await user.save();
-        return res.status(200).json({message:"successfully added to the cart"})
-    }catch(error){
-        return res.status(500).json({message:"unkown error,Please Try Again!",error:error.stack})
-    }
-};
-
-export const updateCart = async(req,res)=>{
-    const {productId,quantity} = req.body
-    try{
-
+    try {
         const user = await User.findById(req.user._id);
-        if(!user){
-            return res.status(404).json({message:"Prduct not find"})
+        if (!user) {
+            return res.status(404).json({ message: "User not found!" });
         }
-        const findProduct = await user.cart.find(Item => Item.productId.toString() === productId.toString());
-        if(!findProduct){
-            return res.status(404).json({message:"Prduct not find"})
+
+        const findProduct = await Product.findById(productId);
+        if (!findProduct) {
+            return res.status(404).json({ message: "Product not found!" });
         }
-        if(quantity > 0 ){
-            user.cart.quantity +=quantity
-            await user.save();
-            return res.status(200).json("you have updated suucessfully!")
+
+        // Check if product already exists in cart
+        const existingProduct = user.cart.find(item => item.product.toString() === productId.toString());
+
+        if (existingProduct) {
+            existingProduct.quantity += quantity;
+        } else {
+            user.cart.push({ product: productId, quantity: quantity });
         }
-        else if((user.cart.quantity >= Math.abs(quantity)) &&  quantity < 0){
-            user.cart.quantity -=quantity;
-            await user.save()
-            return res.status(200).json("you have updated suucessfully!")
-        }
-        else if((user.cart.quantity < Math.abs(quantity)) &&  quantity < 0){
-            user.cart.quantity =0;
-            return res.status(400).json({message:"you have less amount of quanity than you have depricate"});
-            await user.save()
-        }
-        
-    }catch(error){
-        return res.status(500).json({message:"unkown error,Please Try Again!",error:error.stack})
+
+        await user.save();
+        return res.status(200).json({ message: "Product added/updated successfully in the cart!" });
+
+    } catch (error) {
+        return res.status(500).json({ message: "Unknown error, please try again!", error: error.stack });
     }
 };
 
-export const getUserCart = async(req,res)=>{
-    const{userid} = req.params
-    try{
+// Update Cart Function
+export const updateCart = async (req, res) => {
+    const { productId, quantity } = req.body;
 
-    }catch(error){
-        
+    try {
+        const user = await User.findById(req.user._id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found!" });
+        }
+
+        const findProduct = user.cart.find(item => item.product.toString() === productId.toString());
+        if (!findProduct) {
+            return res.status(404).json({ message: "Product not found in cart!" });
+        }
+
+        // Update quantity logic
+        if (quantity > 0) {
+            findProduct.quantity += quantity;
+        } else if (findProduct.quantity >= Math.abs(quantity) && quantity < 0) {
+            findProduct.quantity += quantity; // Subtracting
+            if (findProduct.quantity === 0) {
+                user.cart = user.cart.filter(item => item.product.toString() !== productId.toString());
+            }
+        } else {
+            return res.status(400).json({ message: "Invalid quantity update!" });
+        }
+
+        await user.save();
+        return res.status(200).json({ message: "Cart updated successfully!" });
+
+    } catch (error) {
+        return res.status(500).json({ message: "Unknown error, please try again!", error: error.stack });
     }
-}
+};
+
+// Get User Cart
+export const getUserCart = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const findUser = await User.findById(userId);
+        if (!findUser) {
+            return res.status(404).json({ message: "User not found!" });
+        }
+        if (userId.toString() === req.user._id.toString()) {
+            const userCart = findUser.cart;
+            return res.status(200).json({ message: 'Here is the user cart', userCart });
+        } else {
+            return res.status(403).json({ message: "You are not authorized to see another person's cart!" });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: "Unknown error, please try again!", error: error.stack });
+    }
+};
+
+// Delete User Cart
+export const deleteUserCart = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const findUser = await User.findById(userId);
+        if (!findUser) {
+            return res.status(404).json({ message: "User not found!" });
+        }
+        if (userId.toString() === req.user._id.toString()) {
+            findUser.cart = [];
+            await findUser.save();
+            return res.status(200).json({ message: "Cart deleted successfully!" });
+        } else {
+            return res.status(403).json({ message: "You are not authorized to perform this action!" });
+        }
+
+    } catch (error) {
+        return res.status(500).json({ message: "Unknown error, please try again!", error: error.stack });
+    }
+};
+
+// Delete Specific Product from Cart
+export const deleteProduct = async (req, res) => {
+    const { userId } = req.params;
+    const { productId } = req.body;
+    try {
+        const findUser = await User.findById(userId);
+        if (!findUser) {
+            return res.status(404).json({ message: "User not found!" });
+        }
+        if (userId.toString() === req.user._id.toString()) {
+            findUser.cart = findUser.cart.filter(item => item.product.toString() !== productId.toString());
+            await findUser.save();
+            return res.status(200).json({ message: "Product removed from cart successfully!" });
+        } else {
+            return res.status(403).json({ message: "You are not authorized for this task!" });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: "Unknown error, please try again!", error: error.stack });
+    }
+};
