4d4550a5f9a7f0a0007f688a1978231fef8eeda5
Abel Guta <gutaabel546@gmail.com>
2025-11-02 10:34:00 +0300

    Fixed Bugs

--- ecommerce-backend/models/product.js
+++ ecommerce-backend/models/product.js
@@ -1,52 +1,53 @@
-import mongoose , {Schema} from "mongoose";
-
+import mongoose, { Schema } from "mongoose";
 
 const productSchema = mongoose.Schema({
-    Name:{
-        type:String,
-        required:true,
-        trim:true
+  Name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  ratings: [
+    {
+      userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      rate: {
+        type: Number,
+        min: 1,
+        max: 5,
+      },
     },
-    description:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-    category:{
-        type:String,
-        required:true
-    },
-    comment:{
-        type:String
-    },
-    stock:{
-        type:Number,
-        required:true
-    },
-    ratings:[{
-        userId:{
-            type:Schema.Types.ObjectId,
-            ref:'User'
-        },
-        rate:{
-            type:Number,
-            min:1,
-            max:5
-        }
-    }],
-    createdAt:{
-        type:Date,
-        dafault:Date.now()
-    },
-    updatedAt:{
-        type:Date,
-        dafault:Date.now()
-    }
-})
+  ],
+  createdAt: {
+    type: Date,
+    dafault: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    dafault: Date.now(),
+  },
+});
 
-const Product = mongoose.model('Product',productSchema);
+const Product = mongoose.model("Product", productSchema);
 export default Product;
