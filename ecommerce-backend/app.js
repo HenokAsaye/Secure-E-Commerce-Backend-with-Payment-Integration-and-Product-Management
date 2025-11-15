bff2be043a62afb6d49e28f8f9ac34110bf4cd9e
Henok Asaye <henokasaye77@gmail.com>
2025-11-15 22:27:28 +0300

    refactor: clean up imports and improve code structure in app.js

--- ecommerce-backend/app.js
+++ ecommerce-backend/app.js
@@ -3,20 +3,24 @@ import authRoute from "./routes/authRoute.js";
 import cartRoute from "./routes/cartRoute.js";
 import orderRoute from "./routes/orderRoute.js";
 import paymentRoute from "./routes/paymentRoute.js";
-import productRoute from "./routes/productRoute.js"
+import productRoute from "./routes/productRoute.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from 'url';
 
-const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:true}));
-
-
-app.use("/auth",authRoute);
-app.use("/cart",cartRoute);
-app.use("/order",orderRoute);
-app.use("/payment",paymentRoute);
-app.use("/product",productRoute);
+app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/auth", authRoute);
+app.use("/cart", cartRoute);
+app.use("/order", orderRoute);
+app.use("/payment", paymentRoute);
+app.use("/product", productRoute);
 
 export default app;
