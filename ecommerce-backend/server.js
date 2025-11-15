f1a7196ec78c53e49383e6f44ed8c6e8cc86a56e
Henok Asaye <henokasaye77@gmail.com>
2025-11-15 22:27:39 +0300

    fix: update main entry point and enhance server connection handling

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -1,14 +1,17 @@
-import app from "./app.js"
+import app from "./app.js";
 import dotenv from "dotenv";
 import connectToDb from "./config/db.js";
+import { testStripeConnection } from "./config/stripe.js";
 dotenv.config();
 
-
-connectToDb().then(()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log("server is listening!")
+connectToDb()
+    .then(async () => {
+        await testStripeConnection();
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`🚀 Server is listening on port ${process.env.PORT || 3000}!`);
+        });
     })
-}).catch(error=>console.log("failed to connect to the Db!",error))
+    .catch(error => console.log("❌ Failed to connect to the DB!", error));
 
 
 
